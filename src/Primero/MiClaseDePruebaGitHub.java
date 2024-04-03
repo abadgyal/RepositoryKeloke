c4bd5ad92b73bab4e2fac80512fc3d4a7460a0e9
Marta <Marta@DESKTOP-0PKSBOM>
2024-04-03 10:36:29 +0200

    segundo commit

--- src/Primero/MiClaseDePruebaGitHub.java
+++ src/Primero/MiClaseDePruebaGitHub.java
@@ -5,6 +5,7 @@ public class MiClaseDePruebaGitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("MI PRIMER PROGRAMA");
+		System.out.println("MI PRIMER PROGRAMA badgyaal");
 	}
 
 }
